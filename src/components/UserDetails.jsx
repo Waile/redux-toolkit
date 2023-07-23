2af30037fa212c50450b7c06b2535ee7e4a922ad
Waile Naeem <wailenaeem22@gmail.com>
2023-07-24 00:07:32 +0500

    redux toolkit setup

---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UserDetails = () => {
+  return (
+      <>
+          <div className="content">
+              <div className="admin-table">
+                  <div className="admin-subtitle">List of User Details</div>
+                  <button>Add New User</button>
+              </div>
+          </div>
+   </>
+  )
+}
+
+export default UserDetails
